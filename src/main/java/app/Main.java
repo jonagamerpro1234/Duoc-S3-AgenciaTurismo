979819849dc56a62e5d885aa99b5071d25c51816
SrJss <31166687+jonagamerpro1234@users.noreply.github.com>
2026-06-06 02:32:51 -0400

    se añaden más comentarios javadoc

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,15 +3,32 @@ package app;
 import model.Direccion;
 import model.Empleado;
 
+/**
+ * Clase principal del programa con se encuentra el metodo main.
+ */
 public class Main {
 
+    /**
+     * Metodo principal que permite la ejecucion del programa
+     * @param args
+     */
     public static void main(String[] args) {
+
+        //Primer objeto empleado
         Empleado empleado = new Empleado("Pedro", "Martinez",
                 new Direccion("Buin","Los laureles", "Santiago"), "Jardinero");
 
+        //Segundo objeto empleado
 
+        //tercer objeto empleado
 
+        //Mensajes a mostrar en consola
 
+        //------> 1.
+        System.out.println(empleado.toString());
+        //------> 2.
+        System.out.println(empleado.toString());
+        //------> 3.
         System.out.println(empleado.toString());
 
     }
